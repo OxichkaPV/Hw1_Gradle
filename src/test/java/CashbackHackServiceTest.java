d9057d5da9c5b14d779129764e89236d646ca54c
oxanaP <oxichka@list.ru>
2024-09-09 00:11:03 +0700

    New tests in the brunch testNG

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,42 @@
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void calculateEqualToTheLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void calculateLessThanTheLimitHundred() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void calculateMoreThanTheLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void calculateLessThanTheLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(actual, expected);
+    }
+}
